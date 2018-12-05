f69e5b37cfc428efb7e612f141def57375c39201
cherish <591981210@qq.com>
2018-12-05 20:20:17 +0800

    完成详情列表

--- App.js
+++ App.js
@@ -13,6 +13,8 @@ import { createBottomTabNavigator,createStackNavigator } from 'react-navigation'
 //导入相应的组件
 import HomeScreen from './src/components/home/Home.js'
 import MovieScreen from './src/components/movie/Movie.js'
+import MovieListScreen from './src/components/movie/MovieList.js'
+import MovieInfoScreen from './src/components/movie/MovieInfo.js'
 import MineScreen from './src/components/mine/Mine.js'
 
 //创建首页独立的Navigation
@@ -35,7 +37,7 @@ const HomeStack = createStackNavigator({
 const MovieStack = createStackNavigator({
     Movie: {
         screen:MovieScreen,
-        // 如果是静态的，就可以写死在这里，如果是动态的，不要在这里写，应该在每个组件中进行设置
+        // title如果是静态的，就可以写死在这里，如果是动态的，不要在这里写，应该在每个组件中进行设置
         navigationOptions:{
             headerTitle:'电 影',
             // headerTintColor:'#ff0000',
@@ -45,6 +47,29 @@ const MovieStack = createStackNavigator({
             }
         }
     },
+    MovieList:{
+        screen:MovieListScreen,
+        // 如果是静态的，就可以写死在这里，如果是动态的，不要在这里写，应该在每个组件中进行设置
+        navigationOptions:{
+            // headerTitle:'电影列表',
+            // headerTintColor:'#ff0000',
+            headerTitleStyle :{
+                flex:1,
+                textAlign:'center'
+            }
+        }
+    },
+    MovieInfo:{
+        screen:MovieInfoScreen,
+        navigationOptions:{
+            // headerTitle:'电影详情',
+            // headerTintColor:'#ff0000',
+            headerTitleStyle :{
+                flex:1,
+                textAlign:'center'
+            }
+        }
+    }
 });
 
 
