e2d02a4c0b487150946af0fee67e68895e930ebf
cherish <591981210@qq.com>
2018-12-05 17:35:51 +0800

    底部导航栏

---
+++ src/components/home/Home.js
@@ -0,0 +1,10 @@
+import React,{Component} from 'react'
+import {View,Text} from 'react-native'
+
+export default class Home extends Component{
+    render(){
+        return <View style={{paddingTop:44}}>
+            <Text>Home...</Text>
+        </View>
+    }
+}
